e32665993b97998d28362aaf755a200d20c539d2
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-22 19:41:24 +0300

    22.05.22

---
+++ main.js
@@ -0,0 +1,145 @@
+// function randomNunber(){
+// var num = Math.floor(Math.random()*10);
+// console.log(num);}
+
+// function randomNunber2(num){
+//     num=Math.floor(Math.random()*mum+1)
+    
+
+// }
+
+// function randomNunber3(num){
+// var number=Math.floor(Math.random()*10);
+// if(number==num){
+//     return; true
+// }
+// else{
+//     return; false
+// }
+
+// }
+// randomNunber3()
+
+
+// התרגיל שיעקב עשה בכתה
+// function randomNunber4(){
+//     var arraynew=[];
+//     for(i=0;i<10;i++){
+//     arraynew.push(Math.floor(Math.random()*100));
+// }
+// console.log(arraynew);
+    
+// }
+
+
+// שאלה 53
+// function randomMaxNunber(){
+//   var num=number;
+//   number=Math.floor(Math.random()*10);
+//   if(num>number){
+//       return;num
+//   }
+//   else{
+//       return;number
+//   }
+
+// }
+// randomMaxNunber(Number=prompt())
+
+
+// שאלה 54
+// function randomMinNunber(){
+//     var araaynew=[];
+//     for(var i=0;i<5;i++){
+        
+//         araaynew.push(Math.floor(Math.random()*10));
+//     }
+// console.log(araaynew)
+// }
+// randomMinNunber(araaynew=prompt())
+
+//שאלה 55
+// function sumNumber(){
+//     number=Math.floor(Math.random()*100);
+//     if(num&2==0){
+//         return;Math.min(num,number);
+//     }
+//     else{
+//         return;Math.max(num,number)
+//     }
+// }
+// sumNumber(num,number)
+
+//שאלה 56
+
+// function randomAndValu(){
+//     var araaynew=[];
+//     for(var i=0; i<Math.floor(Math(random)*365);i++){
+
+//             araaynew.push(Math.floor(Math.random()*874));
+//         }
+    
+// }
+// randomAndValu()
+
+//שאלה 44
+// function getArray(newArray=[]){
+//     var num=Math.floor(Math.round()*10);
+//     for(var i=0; i<newArray.length;i++){
+//         if(num==newArray[i]){
+//             console.log(num);
+//         }
+//     }
+// }
+
+//שאלה 45
+
+// function nameChange(names=[""]){
+//     var num=Math.floor(Math.random()*10);
+//     for(var i=0; i<names.length;i++){
+//         if(num==names[i]){
+//             names[i].toUpperCase(i,1);
+//         }
+//     }
+// }
+
+//שאלה 46
+
+// function nameAndAge(){
+//     var name1 = prompt("name1");
+//     var name2 = prompt("name2");
+//     var age1 = +prompt("age1");
+//     var age2 = +prompt("age2");
+//     var num=Math.floor(Math.random()*10);
+//     if(name1[0]==name2[0]){
+//         console.log(num);
+//     }
+//     if(name1.lastIndexOf[i]==name2.lastIndexOf[i]){
+//         console.log("r");
+//     }
+//     else{
+//         console.log(age1,age2);
+//     }
+
+
+// }
+// nameAndAge()
+
+
+//שאלה 47
+// function getTextToRandom(){
+//     var text=[""];
+//     var num=Math.floor(Math.random()*6);
+//     console.log(text[0,num]);
+// }
+
+//שאלה 48
+
+function sumNumber(){
+    var num=prompt();
+    var number= Math.floor(Math.random()*num+1);
+    if(num%3==0){
+        
+        console.log(number);
+    }
+}
